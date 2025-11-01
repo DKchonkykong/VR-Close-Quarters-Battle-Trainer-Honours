2a458ba66e7c49e994dd3ba8e6044d6ebf324f7a
AdrianV2022 <146434555+DKchonkykong@users.noreply.github.com>
2025-11-01 14:28:33 +0000

    updated the way magazine works and removed righthandmagazine script need to next add gravity and test the magazine spawner script

--- Assets/Scripts/MagazineSpawner.cs
+++ Assets/Scripts/MagazineSpawner.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MagazineSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Spawn setup")]
+    public GameObject magazinePrefab;
+    public Transform spawnPoint;       // where the mag appears
+    public bool spawnOnStart = true;
+
+    [Header("Limit")]
+    public bool onlyOneAtATime = true;
+
+    GameObject currentMag;
+
     void Start()
     {
-        
+        if (spawnOnStart)
+            SpawnMagazine();
     }
 
-    // Update is called once per frame
+    // Call this from code or a UnityEvent
+    public void SpawnMagazine()
+    {
+        if (magazinePrefab == null || spawnPoint == null)
+        {
+            Debug.LogWarning("[MagazineSpawner] Missing prefab or spawn point.", this);
+            return;
+        }
+
+        if (onlyOneAtATime && currentMag != null)
+            return;
+
+        currentMag = Instantiate(magazinePrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    // the magazine can tell the spawner if it's destroyed 
+    public void NotifyMagazineDestroyed(GameObject magGO)
+    {
+        if (currentMag == magGO)
+            currentMag = null;
+    }
+
     void Update()
     {
-        
+        // debug: press M to spawn
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            SpawnMagazine();
+        }
     }
 }
