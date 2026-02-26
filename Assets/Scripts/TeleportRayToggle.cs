7c972e7ee1fc9dcc1972ff5491695f2a580ed09a
AdrianV2022 <146434555+DKchonkykong@users.noreply.github.com>
2026-02-26 01:03:09 +0000

    didn't have enough time to have it so hostage can go to safezone sadly might have to do that later on

--- Assets/Scripts/TeleportRayToggle.cs
+++ Assets/Scripts/TeleportRayToggle.cs
@@ -4,46 +4,23 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class TeleportRayToggle : MonoBehaviour
 {
-    [Header("Refs")]
     public XRRayInteractor ray;
-    public LineRenderer lineRenderer;
-    public InputActionReference teleportAction;   // XRI Left/Right Locomotion/Teleport Mode Activate
+    public Behaviour lineVisual; // XRInteractorLineVisual OR LineRenderer
+    public InputActionReference moveAction; // <-- set to XRI .../Move (Vector2)
 
-    [Header("Settings")]
-    [Range(0f, 1f)]
-    public float thumbstickUpThreshold = 0.5f;
+    [Range(0f, 1f)] public float thumbstickUpThreshold = 0.5f;
 
-    InteractionLayerMask originalLayers;
-
-    void Awake()
-    {
-        if (ray != null)
-            originalLayers = ray.interactionLayers;
-    }
-
-    void OnEnable()
-    {
-        if (teleportAction != null)
-            teleportAction.action.Enable();
-    }
-
-    void OnDisable()
-    {
-        if (teleportAction != null)
-            teleportAction.action.Disable();
-    }
+    void OnEnable() => moveAction?.action.Enable();
+    void OnDisable() => moveAction?.action.Disable();
 
     void Update()
     {
-        if (ray == null || teleportAction == null) return;
+        if (!ray || moveAction == null) return;
 
-        Vector2 stick = teleportAction.action.ReadValue<Vector2>();
+        Vector2 stick = moveAction.action.ReadValue<Vector2>();
         bool teleportMode = stick.y > thumbstickUpThreshold;
 
-        // Only interact with Teleport areas while in teleport mode
-        ray.interactionLayers = teleportMode ? originalLayers : new InteractionLayerMask();
-        // Show/hide visual
-        if (lineRenderer != null)
-            lineRenderer.enabled = teleportMode;
+        ray.enabled = teleportMode;
+        if (lineVisual) lineVisual.enabled = teleportMode;
     }
 }
